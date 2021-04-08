968d9d1a4ad1c7c978b8a9ec0cbe8bc8c6e75b44
iamatul369 <68892724+iamatul369@users.noreply.github.com>
2021-04-09 02:09:52 +0530

    Update my.js

--- my app/my.js
+++ my app/my.js
@@ -1,8 +1,14 @@
 let ch = [];
+document.getElementById("upd").style.display = "none";
+
 function update(){
     let tit = document.getElementById("title").value ;
     let desc = document.getElementById("description").value;
-    
+    if(tit=="" || desc=="")
+    {
+        alert("Title and Description must be filled out");
+        return false;
+    }
     
     if(localStorage.getItem('itemJson') == null)
     {
@@ -43,7 +49,8 @@ function updatetab (){
             <th scope=row>${index+1}</th>
             <td>${element[0]}</td>
             <td>${element[1]}</td>
-            <td><button class="btn btn-primary"   onclick="deleted(${index})"  >Delete</button><button class="btn btn-primary" onclick="updt(${index})">Update</button></td>
+            <td><button class="btn btn-primary"   onclick="deleted(${index})"  >Delete</button>
+            <button style="padding -left: 10px;" class="btn btn-primary" onclick="updt(${index})">Update</button></td>
             </tr>
         `;
         
@@ -52,7 +59,16 @@ function updatetab (){
    
 }
 
+let addb = document.getElementById("add1");
+addb.addEventListener("click",update);
+updatetab();
+
+let upd1 = document.getElementById("upd");
+upd1.addEventListener("click",updt1);
+
 function updt(item){
+document.getElementById("add1").style.display = "none";
+    document.getElementById("upd").style.display = "inline";
     arrstr=localStorage.getItem('itemJson');
     arr = JSON.parse(arrstr);
     console.log(item);
@@ -62,44 +78,35 @@ function updt(item){
             document.getElementById("title").value=element[0];
             document.getElementById("description").value=element[1];
         }
-        ch.push(item);
+        ch.push(item); //i am pushing index of the current post to update it by using a global array
         console.log("hii",ch[0]);
      
     });
 }
 function updt1()
 {
-     let im=ch[0];
-        arr.forEach((element , index) => {
-            if(im == index)
-            {
+    
+    let im=ch[0];
+    arr.forEach((element , index) => {
+         if(im == index)
+             {
                 let t =document.getElementById("title").value;
-               let d = document.getElementById("description").value;
+                let d = document.getElementById("description").value;
                 console.log(t,d);
-     
-        arr.splice(im,1,[t,d]);
-        localStorage.setItem('itemJson',JSON.stringify(arr));
-      
-
+                arr.splice(im,1,[t,d]);
+                localStorage.setItem('itemJson',JSON.stringify(arr));
             }
-          });
+        });
           
         updatetab();
         ref();
         ch.pop();
-   
-    }
+}
 
 
-let addb = document.getElementById("add1");
-addb.addEventListener("click",update);
-updatetab();
-
-let upd1 = document.getElementById("upd");
-upd1.addEventListener("click",updt1);
 
 function deleted(item){
-     console.log("deleted",item);
+    console.log("deleted",item);
     arrstr=localStorage.getItem('itemJson');
     arr=JSON.parse(arrstr);
     arr.splice(item,1);
